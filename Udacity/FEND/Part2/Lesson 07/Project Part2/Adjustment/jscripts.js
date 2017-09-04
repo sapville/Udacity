b3a88e3f895b766c5805b177f5ab6604ccc35ed7
Evgenii Fedoseev <abap@sapville.com>
2017-09-04 16:07:49 +0300

    Canvas-off 1

---
+++ Udacity/FEND/Part2/Lesson 07/Project Part2/Adjustment/jscripts.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+    document.getElementById("side-nav").style.width = "500px";
+    /*document.getElementById("main").style.marginLeft = "250px";*/
+    document.getElementById("main").style.opacity = "0";
+}
+
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+    /*document.getElementById("main").style.marginLeft = "0";*/
+    document.getElementById("main").style.opacity = "1";
+}    

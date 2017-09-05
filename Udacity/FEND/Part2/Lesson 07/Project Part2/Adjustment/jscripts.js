f4d53af86ac0bb425e9a00a997590065cb84aa9f
Evgenii Fedoseev <abap@sapville.com>
2017-09-05 11:15:45 +0300

    Off-canvas via class toggle

--- Udacity/FEND/Part2/Lesson 07/Project Part2/Adjustment/jscripts.js
+++ Udacity/FEND/Part2/Lesson 07/Project Part2/Adjustment/jscripts.js
@@ -1,10 +1,13 @@
 
 function openNav() {
-	document.getElementById("drawer").style.transform = "translate(0, 0)";
-	document.getElementById("main").style.opacity = "0.5";
+/*	document.getElementById("drawer").style.transform = "translate(0, 0)";
+	document.getElementById("main").style.opacity = "0.5";*/
+	
+	document.getElementById("drawer").classList.toggle('open');	
 }
 
 function closeNav() {
-	document.getElementById("drawer").style.transform = "translate(-300px, 0)";
-	document.getElementById("main").style.opacity = "1";
+	/*document.getElementById("drawer").style.transform = "translate(-300px, 0)";
+	document.getElementById("main").style.opacity = "1";*/
+	document.getElementById("drawer").classList.remove('open');	
 }    

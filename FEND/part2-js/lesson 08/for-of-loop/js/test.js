813b7bb437026eae57b06538097429d7ce67dd0a
Evgenii Fedoseev <abap@sapville.com>
2017-09-22 20:09:40 +0300

    eslint config

--- FEND/part2-js/lesson 08/for-of-loop/js/test.js
+++ FEND/part2-js/lesson 08/for-of-loop/js/test.js
@@ -3,11 +3,11 @@ function showResult() {
   ///////////////////////////////////////////////////////////////////////////
   /*************************************************************************/
 
-const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
-for (let day of days) {
-  console.log(day.replace(day.charAt(0), day.charAt(0).toUpperCase()));
-}
+  for (let day of days) {
+    console.log(day.replace(day.charAt(0), day.charAt(0).toUpperCase()));
+  }
 
   /****************************************************************************/
   if (output !== null) {

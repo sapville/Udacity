bf59aa5eb968e245a6cf28cdd61af7d8cefff1d8
Evgenii Fedoseev <abap@sapville.com>
2017-09-29 12:30:15 +0300

    part 2 finished

--- FEND/part2-js/lesson 08/using-let-and-const/js/test.js
+++ FEND/part2-js/lesson 08/using-let-and-const/js/test.js
@@ -5,9 +5,9 @@ function showResult() {
 
   const CHARACTER_LIMIT = 255;
   const posts = [
-    "#DeepLearning transforms everything from self-driving cars to language translations. AND it's our new Nanodegree!",
-    "Within your first week of the VR Developer Nanodegree Program, you'll make your own virtual reality app",
-    "I just finished @udacity's Front-End Web Developer Nanodegree. Check it out!"
+    '#DeepLearning transforms everything from self-driving cars to language translations. AND it\'s our new Nanodegree!',
+    'Within your first week of the VR Developer Nanodegree Program, you\'ll make your own virtual reality app',
+    'I just finished @udacity\'s Front-End Web Developer Nanodegree. Check it out!'
   ];
 
   // prints posts to the console

e3eae831d40b10c5597e51358a3c644c5df5efe9
Evgenii Fedoseev <abap@sapville.com>
2017-12-07 16:13:14 +0300

    double cats

--- FEND/part4/lesson06/cat-clicker/js/test.js
+++ FEND/part4/lesson06/cat-clicker/js/test.js
@@ -1,16 +1,32 @@
-class App {
-  constructor () {
+class Cat {
+  constructor (name, id) {
     this.counter = -1;
+    this.name = name;
+    this.id = id;
   }
 
-  imgClick () {
+  catClick () {
     this.counter++;
-    $('.counter h1').text(this.counter);
+    $('h1.' + this.id).text(`${this.name}: ${this.counter}`);
+  }
+}
+
+class App {
+  constructor () {
+    this.cats = [];
+    this.cats.push(new Cat('Kitten', 'cat1'));
+    this.cats.push(new Cat('Shy', 'cat2'));
+  }
+
+  imgClick (id) {
+    this.cats.find(cat => cat.id === id).catClick();
   }
 }
 
 function main () {
   const app = new App();
-  app.imgClick();
-  $('.image img').click( () => {app.imgClick();});
+  app.imgClick('cat1');
+  app.imgClick('cat2');
+  $('img.cat1').click( () => {app.imgClick('cat1');});
+  $('img.cat2').click( () => {app.imgClick('cat2');});
 }
